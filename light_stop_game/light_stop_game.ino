ab7bb25d6d54b0cc876b165f88453a82ccdb472e
Peter Sterckx <pasterckx@gmail.com>
2019-09-29 13:40:23 -0400

    'update'

--- light_stop_game/light_stop_game.ino
+++ light_stop_game/light_stop_game.ino
@@ -1,11 +1,12 @@
 #include <FastLED.h>
-#define NUM_LEDS 10
+#define NUM_LEDS 20
 #define DATA_PIN 5
 #define button 8
 
 CRGB leds[NUM_LEDS];
-int light_speed = 100;
+int light_speed = 1000;
 long stop_pos = 0;
+int brightness = 100;
 
 void setup() {
    FastLED.addLeds<WS2812B, DATA_PIN, RGB>(leds, NUM_LEDS);
@@ -19,14 +20,14 @@ void setup() {
 
 void loop() {
 
-  stop_pos = random(0,10);
-  leds[stop_pos] = CRGB(255,255,255);
+  stop_pos = random(0,NUM_LEDS);
+  leds[stop_pos] = CRGB(brightness,brightness,brightness);
   FastLED.show();
 
   for (int n = 0; n < 5;n++) {
     for (int i = 0;i < NUM_LEDS;i++) {
       if (stop_pos != i) {
-        leds[i] = CRGB(255,255,0);
+        leds[i] = CRGB(brightness,brightness,0);
         FastLED.show();
         delay(light_speed);
 
@@ -38,16 +39,16 @@ void loop() {
         
         leds[i] = CRGB(0,0,0);
       } else {
-        leds[i] = CRGB(255,255,0);
+        leds[i] = CRGB(brightness,brightness,0);
         FastLED.show();
         delay(light_speed);
-        leds[i] = CRGB(255,255,255);
+        leds[i] = CRGB(brightness,brightness,brightness);
       }
     }
   
     for (int i = NUM_LEDS-2;i > 0 ;i--) {
       if (stop_pos != i) {
-        leds[i] = CRGB(255,255,0);
+        leds[i] = CRGB(brightness,brightness,0);
         FastLED.show();
         delay(light_speed);
         
@@ -59,10 +60,10 @@ void loop() {
         
         leds[i] = CRGB(0,0,0);
       } else {
-        leds[i] = CRGB(255,255,0);
+        leds[i] = CRGB(brightness,brightness,0);
         FastLED.show();
         delay(light_speed);
-        leds[i] = CRGB(255,255,255);
+        leds[i] = CRGB(brightness,brightness,brightness);
       }
     }
  }
@@ -74,7 +75,7 @@ void loop() {
 
 void all_on() {
   for (int i = NUM_LEDS-2;i > 0 ;i--) {
-      leds[i] = CRGB(255,255,255);
+      leds[i] = CRGB(brightness,brightness,brightness);
     }
   FastLED.show();
 }
