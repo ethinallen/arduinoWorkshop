c5948f928015366de80fda4117c158c803532903
Peter Sterckx <pasterckx@gmail.com>
2019-09-29 14:09:50 -0400

    'update'

--- light_stop_game/light_stop_game.ino
+++ light_stop_game/light_stop_game.ino
@@ -1,10 +1,10 @@
 #include <FastLED.h>
-#define NUM_LEDS 20
+#define NUM_LEDS 10
 #define DATA_PIN 5
 #define button 8
 
-CRGB leds[NUM_LEDS];
-int light_speed = 1000;
+CRGB leds[NUM_LEDS*3];
+int light_speed = 500;
 long stop_pos = 0;
 int brightness = 100;
 
@@ -12,7 +12,7 @@ void setup() {
    FastLED.addLeds<WS2812B, DATA_PIN, RGB>(leds, NUM_LEDS);
    pinMode(button,INPUT);
 
-   for (int i = 0;i< NUM_LEDS;i++) {
+   for (int i = 0;i< NUM_LEDS*3;i++) {
     leds[i] = CRGB(0,0,0);
    }
 
@@ -30,18 +30,16 @@ void loop() {
         leds[i] = CRGB(brightness,brightness,0);
         FastLED.show();
         delay(light_speed);
-
-        if ( digitalRead(button) == LOW) {
-          all_on();
-          delay(1000);
-          all_off();
-        }
-        
         leds[i] = CRGB(0,0,0);
       } else {
-        leds[i] = CRGB(brightness,brightness,0);
+        leds[i] = CRGB(0,brightness,0);
         FastLED.show();
         delay(light_speed);
+
+         if ( digitalRead(button) == LOW) {
+          win();
+        }
+        
         leds[i] = CRGB(brightness,brightness,brightness);
       }
     }
@@ -50,19 +48,17 @@ void loop() {
       if (stop_pos != i) {
         leds[i] = CRGB(brightness,brightness,0);
         FastLED.show();
-        delay(light_speed);
-        
-        if ( digitalRead(button) == LOW) {
-          all_on();
-          delay(1000);
-          all_off();
-        }
-        
+        delay(light_speed);        
         leds[i] = CRGB(0,0,0);
       } else {
-        leds[i] = CRGB(brightness,brightness,0);
+        leds[i] = CRGB(0,brightness,0);
         FastLED.show();
         delay(light_speed);
+
+        if ( digitalRead(button) == LOW) {
+          win();
+        }
+        
         leds[i] = CRGB(brightness,brightness,brightness);
       }
     }
@@ -86,5 +82,28 @@ void all_off() {
     }
   FastLED.show();
 }
+
+void win() {
+
+  for (int n = 0;n<3;n++){
+    for (int i = NUM_LEDS-2;i > 0 ;i--) {
+        leds[i] = CRGB(brightness,0,0);
+      }
+    FastLED.show();
+    delay(500);
+    
+    for (int i = NUM_LEDS-2;i > 0 ;i--) {
+        leds[i] = CRGB(0,0,0);
+      }
+    FastLED.show();
+    delay(500);
+  }
+
+  light_speed *= .5;
+  stop_pos = random(0,NUM_LEDS);
+
+}
+
+  
 
 
