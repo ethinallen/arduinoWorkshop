0c5e800949363abe4663b5b6ef7465ec5fe6708f
jakabo27 <jat5@g.clemson.edu>
2019-10-04 23:25:23 -0400

    Added LED lighting to the IntroExample

--- IntroExample/IntroExample.ino
+++ IntroExample/IntroExample.ino
@@ -1,7 +1,6 @@
 #define BUTTON_PIN 8
 
 void setup() {
-
   Serial.begin(9600);                                        
   pinMode(BUTTON_PIN, INPUT);                                         
   pinMode(13, OUTPUT); //Use 13 because it has an LED built into the Arduino Uno board
@@ -11,9 +10,18 @@ void setup() {
 
 void loop () {
   
-  //byte buttonValue = digitalRead(BUTTON_PIN);
-  byte buttonValue = analogRead(0);
+  //Read the button value and "print" it to the serial monitor
+  byte buttonValue = digitalRead(BUTTON_PIN);
+  //byte buttonValue = analogRead(0);
   Serial.println(buttonValue);
+
+  //Change the LED
+  if(buttonValue > 0)
+    digitalWrite(13, HIGH);
+  else
+    digitalWrite(13, LOW);
+
+  //Wait a bit 
   delay(200);
   
 } 
